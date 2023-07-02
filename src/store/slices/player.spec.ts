f005cd9fe8d528bb6d04707791692322f9375a18
fabricio <fabriciotailwind@gmail.com>
2023-07-02 09:20:49 -0300

    test(store/slices/player): created unit test

---
+++ src/store/slices/player.spec.ts
@@ -0,0 +1,64 @@
+import { describe, expect, it } from 'vitest'
+import { player as reducer, play, next } from './player'
+
+const exampleState = {
+  course: {
+    modules: [
+      {
+        id: '1',
+        title: 'Iniciando com React',
+        lessons: [
+          { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
+          { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+        ],
+      },
+      {
+        id: '2',
+        title: 'Estrutura da aplicação',
+        lessons: [
+          { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+          { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+        ],
+      },
+    ],
+  },
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+}
+
+describe('Store: Player Slice', () => {
+  it('Should play', () => {
+    const state = reducer(exampleState,  play([1, 2]))
+
+    expect(state.currentModuleIndex).toEqual(1)
+    expect(state.currentLessonIndex).toEqual(2)
+  })
+
+  it('Should be able to play next video on ended', () => {
+    const state = reducer(exampleState, next())
+
+    expect(state.currentModuleIndex).toEqual(0)
+    expect(state.currentLessonIndex).toEqual(1)
+  })
+
+  it('should be able to jump to the next module automatically', () => {
+    const state = reducer({
+      ...exampleState,
+      currentLessonIndex: 1
+    }, next())
+
+    expect(state.currentModuleIndex).toEqual(1)
+    expect(state.currentLessonIndex).toEqual(0)
+  })
+  
+  it('should be not update if not have a next module or lesson', () => {
+    const state = reducer({
+      ...exampleState,
+      currentLessonIndex: 1,
+      currentModuleIndex: 1
+    }, next())
+
+    expect(state.currentModuleIndex).toEqual(1)
+    expect(state.currentLessonIndex).toEqual(1)
+  })
+})
